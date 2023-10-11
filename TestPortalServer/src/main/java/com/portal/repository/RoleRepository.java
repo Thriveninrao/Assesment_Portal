edd03468cd1edb825347b0f5bfba71e1a9d5a084
Shivanagoud Annigeri <sgouda1999@gmail.com>
2023-10-11 18:57:02 +0530

    required update

--- TestPortalServer/src/main/java/com/portal/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.portal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.portal.model.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-}
--- TestPortalServer/src/main/java/com/portal/repository/TestResultRepositories.java
+++
@@ -1,9 +0,0 @@
-package com.portal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.portal.model.assessment.TestResult;
-
-public interface TestResultRepositories extends JpaRepository<TestResult, Long> {
-
-}
